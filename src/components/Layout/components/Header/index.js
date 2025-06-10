1857fa6f265d081dbca5248a0985f7110e63e68e
Mistane <mistanevn@gmail.com>
2025-06-10 22:04:40 +0700

    css search lan 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,33 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("inner")}></div>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img
+            src="https://static.zerochan.net/Shinonome.Ena.full.4318334.jpg"
+            alt="ena"
+          />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Shinonome Ena" spellCheck={false} />
+          <button className={cx("clear")}>
+            X
+          </button>
+          <span className={cx("spinner")}></span>
+          <button className={cx("search-btn")}>
+            O
+          </button>
+        </div>
+        <div className={cx("actions")}>
+          
+        </div>
+      </div>
     </header>
   );
 }
